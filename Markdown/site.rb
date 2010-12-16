6ee02f4b8f7c8b0fb81dceb9484953d5b9449e13
Andrew Tobin <andrew@grrargh.com>
2010-12-16 23:08:42 +1100

    Got markdown working using kramdown as bluecloth wouldn't download the gem.

--- Markdown/site.rb
+++ Markdown/site.rb
@@ -2,17 +2,18 @@ require 'sinatra'
 require 'haml'
 require 'rest_client'
 require 'json'
-require 'cgi'
+require 'kramdown'
 
-DB_ARTICLES = nil
+DB_SPEAKER = nil
 
 configure do
-	DB_ARTICLES = "http://localhost:5984/articles"
+	DB_SPEAKER = "http://localhost:5984/speakers"
 end
 
 configure :production do
-  DB_ARTICLES = "#{ENV['CLOUDANT_URL']}/articles"
+  DB_SPEAKER = "#{ENV['CLOUDANT_URL']}/speakers"
 end
+
 
 set :haml, :format => :html5
 
@@ -24,22 +25,38 @@ get '/article/form' do
 	haml :articleForm
 end
 post '/article/form' do
-	doc_url = "#{DB_ARTICLES}/#{CGI.escape(params[:name])}"
+	doc_url = "#{DB_SPEAKER}/#{CGI.escape(params[:name])}"
 	new_doc = {
 		'name' => params[:name],
 		'email' => params[:mail],
-        'blurb' => params[:blurb]
-	}
+        'outline' => params[:blurb]
+        }
 	RestClient.put doc_url, new_doc.to_json, :content_type => 'application/json'
+    
 	haml :articlePosted
+end
+
+get '/article/view' do
+
+    docs = RestClient.get "#{DB_SPEAKER}/_all_docs"
+
+    r = JSON.parse(docs)
+    
+    @result = r["rows"]
+    
+	haml :articleView
 end
 
 get '/article/view/:name' do
 
-    doc = RestClient.get "#{DB_ARTICLES}/#{params[:name]}"
-    @result = JSON.parse(doc)
+    docs = RestClient.get "#{DB_SPEAKER}/#{CGI.escape(params[:name])}"
+
+    r = JSON.parse(docs)
     
-	haml :articleView
+    @result = r
+    @blurb = Kramdown::Document.new(r["outline"]).to_html
+    
+	haml :articleSingle
 end
 
 not_found do
